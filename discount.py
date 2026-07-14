f8902c57b02fa9e054215c15cb96ac3c6d35fa87
IIS-GHCStudent149 <IIS-GHCStudent149@innovationinsoftware.com>
2026-07-14 15:50:12 -0400

    intro lint failure

--- discount.py
+++ discount.py
@@ -16,15 +16,10 @@ def calculate_discounted_price(price, customer_type, coupon_code=None, is_holida
         price = price * 0.9
 
     elif coupon_code == "SAVE20" and customer_type == "premium":
-
         price = price * 0.8
-
     if is_holiday:
-
         price = price * 0.95
-
     return round(price, 2)
-
 SUPPORTED_COUPON_CODES = {
     "SAVE10": "10% off for all customers",
     "SAVE20": "20% off exclusively for premium customers",
